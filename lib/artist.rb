226937ade072caa8e2f44b7552f3a64fbdf5c84b
Alex Engelhart <asengelhart@yahoo.com>
2020-01-02 20:57:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,14 @@ class Artist
   end
   
   def new_song(song_name, genre)
-    Song.new
+    Song.new(song_name, self, genre)
+  end 
+  
+  def songs 
+    Song.all.select{|song| song.artist == self}
+  end 
+  
+  def genres 
+    
   end 
 end 
