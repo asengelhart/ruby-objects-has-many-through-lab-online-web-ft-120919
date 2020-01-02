5e4fa57164a1737c89885aa197610e8e2785d440
Alex Engelhart <asengelhart@yahoo.com>
2020-01-02 21:27:22 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,17 @@
 class Patient 
   attr_reader :name 
   @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(doctor, date)
+    Appointment.new()
+  end 
 end 
